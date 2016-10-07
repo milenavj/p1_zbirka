e8750d8c3a66f2ad679691d39a8bb7adccf6177a
milenavj <milena@matf.bg.ac.rs>
2016-10-07 17:47:24 +0200

    deo onoga sto je jovana bila pregazila

--- resenja/1_KontrolaToka/1.1_UvodniZadaci/praktikumi4/1_01.c
+++ resenja/1_KontrolaToka/1.1_UvodniZadaci/praktikumi4/1_01.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 int main()
@@ -7,26 +6,25 @@ int main()
      Svaka promenljiva u programu mora biti deklarisana na 
      pocetku main funkcije. Deklaracija se sastoji iz naziva 
      promenljive (u ovom slucaju n) ispred kog se navodi tip 
-     promenljive (u ovom slucaju celobrojni tip, int). Svaka 
-     deklaracija zavrsava se simbolom ";".   
+     promenljive (u ovom slucaju celobrojni tip, int). 
   */
   
   int n; 
   
   /* 
      Vrednost promenljive se ucitava pomocu funkcije scanf koja 
-     je, kao i funkcija printf, definisana u standardnoj biblioteci 
-     stdio.h. Argumenti funkcije scanf. koji se navode u zagradama 
+     je, kao i funkcija printf, sastavni deo standardne biblioteke. 
+     Argumenti funkcije scanf koji se navode u zagradama 
      ( i ) i razdvajaju zarezima, oznacavaju sledece:
      "%d" - format za tip podatka koji ce biti ucitan 
             (%d za int, svaki tip ima svoj format) 
       &n  - adresa promenljive x (o adresama ce biti vise 
-            reci u narednim poglavljima).
+            reci u narednim zadacima).
       
      Ucitavanje se vrsi sa standardnog ulaza (obicno tastatura).
    */
   printf("Unesite ceo broj: ");
-  scanf("%d", &n);                                                                   
+  scanf("%d", &n);                                            
   
   /* 
      Funkcija printf ispisuje tekst "Uneti broj: ", a nakon toga, 
@@ -36,7 +34,7 @@ int main()
   /* Umesto formata %d, ispisuje vrednost izraza n*n. */
   printf("Kvadrat: %d\n", n*n); 
   /* Umesto formata %d, ispisuje vrednost izraza n*n*n. */                                   
-  printf("Kub: %d\n", n*n*n);                                        
+  printf("Kub: %d\n", n*n*n);                                         
   
-  return 0;
+return 0;
 }
