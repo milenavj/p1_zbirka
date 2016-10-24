3f5279232413d44d1b86012973d87f6c81969836
danijela <danijela@danijela-PC.(none)>
2016-10-24 12:24:06 +0200

    preimenovanje zad

---
+++ resenja/1_KontrolaToka/1.3_Petlje/1.3_12.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+int main()
+{
+   int n;
+   /* Oznaka broja koji unosimo u jednoj iteraciji petlje. */
+   int x;
+   int suma_poz;
+   int suma_neg;
+   /* Brojac. */
+   int i;
+      
+   printf("Unesi pozitivan ceo broj:");
+   scanf("%d",&n);
+   
+   if (n < 0)
+   {
+	   printf("Neispravan unos.\n");
+	   return -1;
+   }
+
+   /* Promenljivama koje ce sadrzati sume se pre ulaska u petlju 
+    * dodeljuje 0 (neutral za sabiranje).
+    */
+   suma_poz=0;  
+   suma_neg=0;  
+   i=0;
+   
+   printf("Unesite brojeve: ");
+   while(i<n)
+   {
+       scanf("%d", &x);
+       
+       if (x<0)
+          suma_neg+=x;
+       else
+          suma_poz+=x;
+          
+       i++;
+   }
+   
+   printf(" Suma pozitivnih: %d\n Suma negativnih: %d\n",suma_poz,suma_neg);
+   return 0;
+}
---
+++ resenja/1_KontrolaToka/1.3_Petlje/1.3_7.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void){
+
+    int n, n_abs;
+    float x;
+    float vrednost;
+    unsigned exp;
+    
+    printf("Unesite redom brojeve x i n: ");
+    scanf("%f %d", &x, &n );
+
+    /* Pocetna vrednost stepena koji se racuna. */
+    vrednost=1;
+
+    /* Stepenovanje. */
+    n_abs=abs(n);
+    for(exp=1; exp<=n_abs; exp++)
+        vrednost=vrednost*x;
+   
+    /* Ukoliko je stepen bio negativan treba
+     * odrediti 1/x^n, sto je zapravo 1/vrednost. */
+    if(n<0){
+      printf("%.3f\n",1/vrednost);
+    }
+    else{
+      printf("%.3f\n",vrednost);
+    }
+    
+    return 0;
+}
