e25282183ddbc85a89149a644d1cf372ad3a1f7b
milenavj <milena@matf.bg.ac.rs>
2016-11-17 21:59:08 +0100

    branislavina izmena

--- resenja/1_KontrolaToka/1.3_Petlje/1.3_46.c
+++ resenja/1_KontrolaToka/1.3_Petlje/1.3_46.c
@@ -1,43 +1,42 @@
 #include <stdio.h>
 
-int main()
-{
-  /* Promenljiva i je brojac, promenljiva S cuva izracunatu sumu,
-     promenljiva znak moze bito 1 ili -1 i odredjuje znak trenutnog 
-     clana sume, a promenljiva clan je tekuci clan niza. */
-  int i, znak, n;
-  float S;
-  float x, clan;
+int main () {
+    /* Promenljiva i je brojac , promenljiva S cuva izracunatu sumu ,
+    promenljiva znak moze bito 1 ili -1 i odredjuje znak trenutnog
+    clana sume , a promenljiva clan je tekuci clan niza. */
 
-  printf("Unesite x i n: ");
-  scanf("%f%d", &x, &n);
+    int i, znak , n;
+    float S;
+    float x, clan;
+    printf(" Unesite x i n: ");
+    scanf("%f%d", &x, &n);
 
-  if (n <= 0) {
-    printf("Neispravan unos.\n");
-    return -1;
-  }
+    if (n <= 0) {
+        printf(" Neispravan unos .\n");
+        return -1;
+    }
 
+    S = 1;
+    clan = 1;
+    i = 1;
+    znak = -1;
 
-  S = 0;
-  clan = 1;
-  i = 1;
-  znak = -1;
+    /* Kako clanovi sume mogu biti negativni , potrebno je posmatrati
+    apsolutnu vrednost clana. */
+    while (i <= 2 * n - 1) {
 
-  /* Kako clanovi sume mogu biti negativni, potrebno je posmatrati
-     apsolutnu vrednost clana. */
-  while (i <= 2 * n) {
-    S = S + clan;
+        /* Promena znaka. */
+        /* Svaki clan suma se od prethodnog clana razlikuje za
+        x^2/(i*(i+1)). */
+        clan = clan * x * x / (i * (i + 1));
+        clan *= znak;
+        
+        S = S + clan;
 
-    /* Promena znaka. */
-    /* Svaki clan suma se od prethodnog clana razlikuje za
-       x^2/(i*(i+1)). */
-    clan = clan * x * x / (i * (i + 1));
-    clan *= znak;
+        i += 2;
+    }
 
-    i += 2;
-  }
+    printf("S=%f\n", S);
 
-  printf("S=%f\n", S);
-
-  return 0;
+    return 0;
 }
