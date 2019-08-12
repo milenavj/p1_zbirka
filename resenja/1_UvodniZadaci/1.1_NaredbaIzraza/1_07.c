b5463d16c71f4bdacf023bdf2c5c755361a4a5f4
Milena Vujosevic Janicic <milena@matf.bg.ac.rs>
2019-08-13 00:33:34 +0200

    Formatiranje prvog poglavlja

--- resenja/1_UvodniZadaci/1.1_NaredbaIzraza/1_07.c
+++ resenja/1_UvodniZadaci/1.1_NaredbaIzraza/1_07.c
@@ -24,12 +24,12 @@ int main() {
 
   /* Pretvaranje razlike u minutama u razliku u satima i minutima.
      Razlika u satima se dobija celobrojnim deljenjem broja minuta
-     sa 60.
-     Preostali broj minuta se dobija kao ostatak pri deljenju sa 60. */
+     sa 60. Preostali broj minuta se dobija kao ostatak pri 
+     deljenju sa 60. */
   duzina_sat = duzina / 60;
   duzina_minut = duzina % 60;
   
-  /* II nacin: duzina_minut = duzina - duzina*60; */
+  /* II nacin: duzina_minut = duzina - duzina * 60; */
   
   /* Ispis rezultata. */
   printf("Duzina trajanja leta: %u h i %u min\n", duzina_sat,
