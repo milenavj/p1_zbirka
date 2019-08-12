bbe33656692f15bf424e7098bc9d61e2a7a3f4a9
Milena Vujosevic Janicic <milena@matf.bg.ac.rs>
2019-08-12 23:28:42 +0200

    dodata uputstva za kompajliranje

--- resenja/1_UvodniZadaci/1.1_NaredbaIzraza/1_01.c
+++ resenja/1_UvodniZadaci/1.1_NaredbaIzraza/1_01.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+/* Prevodjenje programa program.c na Linux operativnom sistemu 
+  koriscenjem kompajlera gcc vrsi se iz terminala komandom 
+  gcc program.c
+  Ukoliko program.c ne sadrzi greske, kompajler ce napraviti 
+  izvrsnu verziju programa koja ce se zvati a.out 
+  Ovaj program se moze pokrenuti iz terminala navodjenjem komande
+  ./a.out
+  Ukoliko se program prevede na sledeci nacin
+  gcc program.c -o program
+  onda ce izvrsna verzija biti imenovana nazivom koji je dat nakon 
+  -o opcije (u ovom slucaju je to rec program) tako da se pokretanje 
+  iz terminala moze uraditi navodjenjem komande 
+  ./program 
+*/
+
 int main() {
   /* Ispis trazene poruke. Na kraju poruke se ispisuje novi red. */
   printf("Zdravo svima!\n");
